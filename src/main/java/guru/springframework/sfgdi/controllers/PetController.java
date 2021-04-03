76bd7c1a8daa3581f792e8dbec89c271a82829cd
svangati <shilpa.vangati@optum.com>
2021-04-03 15:06:49 -0500

    Fixed errors

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,11 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     private final PetService petService;
 
